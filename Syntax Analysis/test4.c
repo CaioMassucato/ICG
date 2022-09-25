1c016df78b79d2d94fdb13436fa7e79243213ae5
CaioMassucato <caiobernardon@gmail.com>
2022-09-25 19:43:46 -0300

    Semantic and Syntax

---
+++ Syntax Analysis/test4.c
@@ -0,0 +1,14 @@
+//with error - mispelled keyword 'while' and unmatched paranthesis
+#include<stdio.h>
+#define x 3
+int main()
+{
+	int a=4;
+	while(a<10)
+	{
+		printf("%d",a);
+		int j=1;
+		while(j<=4)
+			j++;
+	}	//unmatched paranthesis
+}
